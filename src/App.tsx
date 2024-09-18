23446c40b1e4b3b5635e34b6e35c8c67541e1988
OGBiggieCheese <106595700+OGBiggieCheese@users.noreply.github.com>
2024-09-18 13:56:53 -0300

    [Wip] Add router and film screen
    Created router in app.tsx and film screen file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './screens/homepage'; 
+import Film from './screens/film/film';
 
 function App() {
   return (
@@ -8,7 +10,13 @@ function App() {
      
       </header>
       <main>
-        <Homepage /> 
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Homepage/>}></Route>
+            <Route path='/film/:filmID' element={<Film/>}></Route>
+          </Routes>
+        </BrowserRouter>
+ 
       </main>
       <footer>
         
---
+++ src/screens/film/film.tsx
@@ -0,0 +1,14 @@
+import './film.scss';
+
+
+
+function Film() {
+
+  return (
+    <>
+     <h2>FILM</h2>
+    </>
+  )
+}
+
+export default Film
